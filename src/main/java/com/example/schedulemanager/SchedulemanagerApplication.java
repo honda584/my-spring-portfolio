d4d73b681b4e673cd9f9e16a43ed5f72d91093f4
honda584 <mtdnh88@gmail.com>
2025-05-09 15:51:30 +0900

    初回コミット

---
+++ src/main/java/com/example/schedulemanager/SchedulemanagerApplication.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/schedulemanager/ScheduleManagerApplication.java
+package com.example.schedulemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulemanagerApplication.class, args);
+    }
+}
